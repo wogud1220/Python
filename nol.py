33873b08f9f39bd51137e544e5a1669d4b882126
wogud1220 <yoonwogud@gmail.com>
2022-10-14 04:24:40 +0900

    asdf

--- nol.py
+++ nol.py
@@ -1,6 +1,12 @@
-import turtle
-#t=turtle.Turtle()
+def want():
+    nol_want=int(input('상명랜드에 오신 것을 환영합니다~!\n\
+1.바이킹\n 2.롤러코스터\n 3.자이로드롭\n 4.번지 점프\n 5.회전 목마\n\
+6.범퍼카\n 7.귀신의 집\n 중에서 제일 먼저 타고 싶은 놀이기구의 번호를 입력하세요.\n'))
+    return nol_want
 
+        
+dic={1:'바이킹',2:' 롤러코스터', 3: '자이로드롭', 4: '번지 점프', 5: '회전 목마',\
+    6:'범퍼카', 7: '귀신의 집'}
 number=int(input('안녕하세요 놀이공원에 오신 것을 환영합니다\n \n몇 명이서 오셨나요?\n'))
 print(number,'명이서 오셨군요~!!\n\n')
 number=int(number)
@@ -16,24 +22,25 @@ if(child=='네'):
     print('가격은 총',(number-child1)*30000 + child1*10000,'원에서 할인하여',
     ((number-child1)*30000 + child1*10000) -
     int((((number-child1)*30000 + child1*10000)*child1*10/100)),'입니다.\n\n')
+    if(1 <= child1 and (want()==1 or 2 or 3 or 4)):
+        print('죄송하지만 고객님이 타시려는 놀이기구들은 키와 몸무게 제한이 있는\
+놀이기구 입니다.\n 키:100cm 초과, 몸무게는 25kg 초과할 것\n 아이의 키와 몸무게를 입력해주시겠어요?')
+        for i in range(1,child1+1,1):
+            height=int(input('%d째 아이의 키: '%i))
+            weight=int(input('%d째 아이의 몸무게: '%i))
+            if(height <= 100 or weight <= 25):
+                print('아이의 신체조건이 맞지 않습니다.')
+                break
+        else:
+            print('\n아이들의 신체조건을 모두 입력받았습니다.')
+    
+
+                
 elif(child=='아니오'):
     print('아이가 없으시군요~ 알겠습니다.\n')
-   
-dic={1:'바이킹',2:' 롤러코스터', 3: '자이로드롭', 4: '번지 점프', 5: '회전 목마',\
-     6:'범퍼카', 7: '귀신의 집'}
-nol_want=int(input('상명랜드에 오신 것을 환영합니다~!\n\
- 1.바이킹\n 2.롤러코스터\n 3.자이로드롭\n 4.번지 점프\n 5.회전 목마\n\
- 6.범퍼카\n 7.귀신의 집\n 중에서 제일 먼저 타고 싶은 놀이기구의 번호를 입력하세요.\n'))
+    want()
 
-if(1 <= child1 and (nol_want==1 or 2 or 3 or 4)):
-    print('죄송하지만 고객님이 타시려는 놀이기구들은 키와 몸무게 제한이 있는\
-놀이기구 입니다.\n 아이의 키와 몸무게를 입력해주시겠어요?')
-    for i in range(1,child1+1,1):
-        height=int(input('%d째 아이의 키: '%i))
-        weight=int(input('%d째 아이의 몸무게: '%i))
-
-               
-import turtle as t
+'''import turtle as t
 
 t.hideturtle()
 t.penup()
@@ -83,7 +90,7 @@ t.pendown()
 t.showturtle()
     
     
-#print('네 그러면 먼저', dic[(nol_want)], '쪽으로 안내해드리겠습니다.')
+#print('네 그러면 먼저', dic[(nol_want)], '쪽으로 안내해드리겠습니다.')'''
 
 
 # 자녀의 키, 몸무게, 기구제한
