a452f17fa6cd0d772e9d3d53fdc3aeefaff60644
wogud1220 <yoonwogud@gmail.com>
2022-10-18 00:53:30 +0900

    ㅁㄴㅇㄹ

--- nol.py
+++ nol.py
@@ -1,8 +1,3 @@
-def want():
-    nol_want=int(input('상명랜드에 오신 것을 환영합니다~!\n\
-1.바이킹\n 2.롤러코스터\n 3.자이로드롭\n 4.번지 점프\n 5.회전 목마\n\
-6.범퍼카\n 7.귀신의 집\n 중에서 제일 먼저 타고 싶은 놀이기구의 번호를 입력하세요.\n'))
-    return nol_want
 
         
 dic={1:'바이킹',2:' 롤러코스터', 3: '자이로드롭', 4: '번지 점프', 5: '회전 목마',\
@@ -11,7 +6,6 @@ number=int(input('안녕하세요 놀이공원에 오신 것을 환영합니다\
 print(number,'명이서 오셨군요~!!\n\n')
 number=int(number)
 print('15세 이상은 3만원, 15세 미만 아이로 취급하여 만원입니다.\n')
-
 
 child=input('혹시 그 중에 아이가 있으신가요? 네 or 아니오\n')
 if(child=='네'):
@@ -22,7 +16,16 @@ if(child=='네'):
     print('가격은 총',(number-child1)*30000 + child1*10000,'원에서 할인하여',
     ((number-child1)*30000 + child1*10000) -
     int((((number-child1)*30000 + child1*10000)*child1*10/100)),'입니다.\n\n')
-    if(1 <= child1 and (want()==1 or 2 or 3 or 4)):
+    print('상명랜드에 오신 것을 환영합니다~!\n\
+    1.바이킹\n 2.롤러코스터\n 3.자이로드롭\n 4.번지 점프\n 5.회전 목마\n\
+    6.범퍼카\n 7.귀신의 집\n 중에서 제일 먼저 타고 싶은 놀이기구의 번호를 입력하세요.\n')
+    nol_sunseo=[]
+    for j in range(4):
+        nol_turn=int(input('타고 싶은 놀이기구를 입력하고 엔터를 누르세요\n'))
+        nol_sunseo.append(nol_turn)
+        print(nol_sunseo)
+
+    if(1 <= child1 and (nol_sunseo[0]==1 or 2 or 3 or 4)):
         print('죄송하지만 고객님이 타시려는 놀이기구들은 키와 몸무게 제한이 있는\
 놀이기구 입니다.\n 키:100cm 초과, 몸무게는 25kg 초과할 것\n 아이의 키와 몸무게를 입력해주시겠어요?')
         for i in range(1,child1+1,1):
@@ -30,17 +33,16 @@ if(child=='네'):
             weight=int(input('%d째 아이의 몸무게: '%i))
             if(height <= 100 or weight <= 25):
                 print('아이의 신체조건이 맞지 않습니다.')
-                break
-        else:
-            print('\n아이들의 신체조건을 모두 입력받았습니다.')
+                continue;
+            else:
+                print('\n아이들의 신체조건이 탑승조건에 만족합니다.')
     
 
                 
 elif(child=='아니오'):
     print('아이가 없으시군요~ 알겠습니다.\n')
-    want()
 
-'''import turtle as t
+import turtle as t
 
 t.hideturtle()
 t.penup()
@@ -54,7 +56,7 @@ t.pendown()
 t.write("바이킹", move=False, align="center", font=("arial",20,"bold"))
 
 t.penup()
-t.setpos(300,-150)
+t.setpos(300,-100)
 t.pendown()
 t.write("롤러코스터", move=False, align="center", font=("arial",20,"bold"))
 
@@ -74,7 +76,7 @@ t.pendown()
 t.write("회전 목마", move=False, align="center", font=("arial",20,"bold"))
 
 t.penup()
-t.setpos(-400,50)
+t.setpos(-300,50)
 t.pendown()
 t.write("범퍼카", move=False, align="center", font=("arial",20,"bold"))
 
@@ -88,10 +90,26 @@ t.setpos(0,-390)
 t.shape('circle')
 t.pendown()
 t.showturtle()
-    
-    
-#print('네 그러면 먼저', dic[(nol_want)], '쪽으로 안내해드리겠습니다.')'''
+
+t.pensize(3)
+n=0
+k=0
 
 
-# 자녀의 키, 몸무게, 기구제한
-# 터틀 약도 (건물, 시간,)
+while(n<4):
+    if(nol_sunseo[k]==1):
+        t.goto(300,-350)
+    elif(nol_sunseo[k]==2):
+        t.goto(300,-350)
+    elif(nol_sunseo[k]==3):
+        t.goto(300,-100)
+    elif(nol_sunseo[k]==4):
+        t.goto(200,200)
+    elif(nol_sunseo[k]==5):
+        t.goto(-100,300)
+    elif(nol_sunseo[k]==6):
+        t.goto(-350,200)
+    elif(nol_sunseo[k]==7):
+        t.goto(-300,-200)
+    n+=1
+    k+=1
