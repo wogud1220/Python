fa417d6cc5d5efa2fb6bb293586c61e80b13a080
wogud1220 <yoonwogud@gmail.com>
2022-10-21 16:51:33 +0900

    asdff

--- Algorithm/team/nol.py
+++ Algorithm/team/nol.py
@@ -20,6 +20,8 @@ if(child=='네'):
  1.바이킹\n 2.롤러코스터\n 3.자이로드롭\n 4.번지 점프\n 5.회전 목마\n\
  6.범퍼카\n 7.귀신의 집\n 등 다양한 놀이기구가 있습니다!!\n')
     nol_choice=[]
+    print('놀이기구 탑승은 최대 4번까지 가능하므로 타고 싶은 놀이기구를 4개만 선택\
+해주세요~~!\n')
     for j in range(1,5,1):
         nol_turn=int(input('%d번째로 타고 싶은 놀이기구를 입력하고 엔터를 누르세요\n' %j))
         nol_choice.append(nol_turn)
@@ -30,8 +32,8 @@ if(child=='네'):
         print('죄송하지만 고객님이 타시려는 놀이기구들은 키와 몸무게 제한이 있는\
 놀이기구 입니다.\n 키:100cm 초과, 몸무게는 25kg 초과할 것\n 아이의 키와 몸무게를 입력해주시겠어요?')
         while(i<=child1):
-            height=int(input('%d째 아이의 키: '%i))
-            weight=int(input('%d째 아이의 몸무게: '%i))
+            height=float(input('%d째 아이의 키: '%i))
+            weight=float(input('%d째 아이의 몸무게: '%i))
             if(height <= 100 or weight <= 25):
                 print('아이의 신체조건이 맞지 않습니다. 다시 입력해주세요.')
                 continue
@@ -47,57 +49,67 @@ elif(child=='아니오'):
  1.바이킹\n 2.롤러코스터\n 3.자이로드롭\n 4.번지 점프\n 5.회전 목마\n\
  6.범퍼카\n 7.귀신의 집\n 등 다양한 놀이기구가 있습니다!!\n')
     nol_choice=[]
+    print('놀이기구 탑승은 최대 4번까지 가능하므로 타고 싶은 놀이기구를 4개만 선택\
+해주세요~~!\n')
     for j in range(1,5,1):
         nol_turn=int(input('%d번째로 타고 싶은 놀이기구를 입력하고 엔터를 누르세요\n' %j))
         nol_choice.append(nol_turn)
         print(nol_turn,dic[nol_turn],'\n','순서 :',nol_choice)
 
 
-import turtle as t
+print('네 여기 약도 그려드렸습니다. 받으시고 즐거운 시간 되시길 바랍니다~')
+
+import turtle
+scr = turtle.Screen()
+scr.setup(100,100)
+scr.bgpic("smu.gif")
+scr.update()
+
+t=turtle.Turtle()
 
 t.hideturtle()
 t.penup()
-t.setpos(0,-390)
+t.setpos(0,-190)
 t.pendown()
 t.write("매표소 (현재위치)", move=False, align="center", font=("arial",20,"bold"))
 
 t.penup()
-t.setpos(300,-350)
+t.setpos(200,-250)
 t.pendown()
-t.write("바이킹", move=False, align="center", font=("arial",20,"bold"))
+t.write("바이킹", move=False, align="center", font=("arial",15,"bold"))
 
 t.penup()
-t.setpos(300,-150)
+t.setpos(250,0)
 t.pendown()
-t.write("롤러코스터", move=False, align="center", font=("arial",20,"bold"))
+t.write("롤러코스터", move=False, align="center", font=("arial",15,"bold"))
 
 t.penup()
-t.setpos(200,200)
+t.setpos(150,200)
 t.pendown()
-t.write("자이로드롭", move=False, align="center", font=("arial",20,"bold"))
+t.write("자이로드롭", move=False, align="center", font=("arial",15,"bold"))
 
 t.penup()
-t.setpos(-100,300)
+t.setpos(-100,250)
 t.pendown()
-t.write("번지점프", move=False, align="center", font=("arial",20,"bold"))
+t.write("번지점프", move=False, align="center", font=("arial",15,"bold"))
 
 t.penup()
-t.setpos(-350,200)
+t.setpos(-250,150)
 t.pendown()
-t.write("회전 목마", move=False, align="center", font=("arial",20,"bold"))
+t.write("회전 목마", move=False, align="center", font=("arial",15,"bold"))
 
 t.penup()
-t.setpos(-300,50)
+t.setpos(-300,0)
 t.pendown()
-t.write("범퍼카", move=False, align="center", font=("arial",20,"bold"))
+t.write("범퍼카", move=False, align="center", font=("arial",15,"bold"))
 
 t.penup()
-t.setpos(-350,-200)
+t.setpos(-250,-150)
 t.pendown()
-t.write("귀신의 집", move=False, align="center", font=("arial",20,"bold"))
+t.write("귀신의 집", move=False, align="center", font=("arial",15,"bold"))
 t.penup()
 
-t.setpos(0,-390)
+t.setpos(0,-190)
 t.shape('circle')
 t.pendown()
 t.showturtle()
@@ -106,22 +118,22 @@ t.pensize(3)
 
 k=0
 n=0
-while(n<5):
+while(n<4):
     t.color('black')
     if(nol_choice[k]==1):
-        t.goto(300,-350)
+        t.goto(200,-250)
     elif(nol_choice[k]==2):
-        t.goto(300,-150)
+        t.goto(250,0)
     elif(nol_choice[k]==3):
-        t.goto(200,200)
+        t.goto(150,200)
     elif(nol_choice[k]==4):
-        t.goto(-100,300)
+        t.goto(-100,250)
     elif(nol_choice[k]==5):
-        t.goto(-350,200)
+        t.goto(-250,150)
     elif(nol_choice[k]==6):
-        t.goto(300,50)
+        t.goto(-300,0)
     elif(nol_choice[k]==7):
-        t.goto(-350, -200)
+        t.goto(-250, -150)
     t.color('red')
     t.write(n+1, move=True, align="left", font=("arial",30,"bold"))
     k+=1
--- Algorithm/test.py
+++
@@ -1,9 +0,0 @@
-def factorial(num):
-    if num<=1:
-        return num
-    else :
-        return num * factorial(num-1)
-
-print(factorial(4))
-print(factorial(3))
-
