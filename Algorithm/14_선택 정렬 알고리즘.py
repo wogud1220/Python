921db128ba6c1bcae8a75516c6bae1126255fa79
wogud1220 <yoonwogud@gmail.com>
2022-12-20 08:12:29 +0900

    ,

--- Algorithm/14_선택 정렬 알고리즘.py
+++ Algorithm/14_선택 정렬 알고리즘.py
@@ -9,7 +9,7 @@ def find_min_idx(a, start):
 
 v = [2, 4, 5, 1, 3]
 for i in range(len(v)-1):
-    min_idx = find_min_idx(v,i)
+    min_idx = find_min_idx(v, i)
     v[i], v[min_idx] = v[min_idx], v[i]
 
 print(v)
