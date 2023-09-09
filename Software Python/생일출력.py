bc2ca19e21c5b48aa0974bd4e23446c683cc22aa
윤재형 <yoonwogud@gmail.com>
2023-09-09 22:25:48 +0900

    a

--- Software Python/생일출력.py
+++ Software Python/생일출력.py
@@ -3,6 +3,6 @@ y = birthday[0:2]
 m = birthday[2:4]
 d = birthday [4:]
 print('birthday is')
-print('year = ', y)
+print('year = '+ y)
 print('Month = ', m)
 print('day = ', d)
---
+++ new.py
@@ -0,0 +1,2 @@
+a='python'
+print(a*2)
