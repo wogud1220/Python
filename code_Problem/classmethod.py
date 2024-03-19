be8d9a0269161753b8089e41425560cb928b2dbb
wogud1220 <yoonwogud@gmail.com>
2024-03-19 14:57:25 +0900

    method

---
+++ code_Problem/classmethod.py
@@ -0,0 +1,30 @@
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    @classmethod
+    def from_string(cls, string_params):
+        params_list = string_params.split(",")
+        name = params_list[0]
+        email = params_list[1]
+        password = params_list[2]
+    # 인스턴스 생성 후 리턴
+        return User(name, email, password)
+    
+    @classmethod
+    def from_list(cls, list_params):
+        name = list_params[0]
+        email = list_params[1]
+        password = list_params[2]
+        return cls(name, email, password)
+    
+
+# 유저 생성 및 초기값 설정
+younghoon = User.from_string("강영훈,younghoon@codeit.kr,123456")
+
+yoonsoo = User.from_list(["이윤수", "yoonsoo@codeit.kr", "abcdef"])
+
+print(younghoon.name, younghoon.email, younghoon.password)
+print(yoonsoo.name, yoonsoo.email, yoonsoo.password)
